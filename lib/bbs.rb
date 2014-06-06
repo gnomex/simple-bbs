855510b1e9a5a13ddcaba17bbbceefae7dade260
Kenner Kliemann <kenner.hp@gmail.com>
2014-06-06 17:09:03 -0300

    Simple example with echo server

--- lib/bbs.rb
+++ lib/bbs.rb
@@ -1,5 +1,24 @@
+require 'rubygems'
+require 'active_record'
 require 'eventmachine'
+require 'yaml'
+
+require './bbs/echo_server'
+
+dbconfig = YAML::load(File.open('db/database.yml'))
+ActiveRecord::Base.establish_connection(dbconfig)
+ActiveRecord::Base.logger = Logger.new(STDERR)
 
 module BBS
   VERSION = "0.0.1"
+
+  def BBS.run_echo_server
+    EventMachine.run do
+      # hit Control + C to stop
+      Signal.trap("INT")  { EventMachine.stop }
+      Signal.trap("TERM") { EventMachine.stop }
+
+      EventMachine.start_server("localhost", 10000, EchoServer)
+    end
+  end
 end
---
+++ lib/bbs/echo_server.rb
@@ -0,0 +1,12 @@
+module BBS
+  class EchoServer < EM::Connection
+    def receive_data(data)
+      puts data.to_s
+      if data.strip =~ /exit$/i
+        EventMachine.stop
+      else
+        send_data(data)
+      end
+    end
+  end
+end
