191997cd89dba2257c948dc62cea46ef23ec69db
Kenner Kliemann <kenner.hp@gmail.com>
2014-06-09 20:13:15 -0300

    More features for posts controller

--- lib/controller/posts_controller.rb
+++ lib/controller/posts_controller.rb
@@ -1,14 +1,27 @@
 module BBS
-  class PostsController
-    def initialize(args = {})
-      @post = Post.new(args)
+ class PostsController
+
+    def create(args = {})
+      if validate_params(args)
+        @post = Post.new(args)
+        @post if @post.save
+      else
+        nil
+      end
     end
 
-    def delete(user)
-      if @post.is_owner?(user) || user.admin?
-        @post.destroy
+    def delete(post_id, user)
+      @post = Post.find_by(post_id: post_id)
+
+      unless @post.nil?
+        if @post.is_owner?(user) or user.admin?
+          @post.destroy
+          "Deleted!"
+        else
+          "You do not have permission to delete!"
+        end
       else
-        "You do not have permission to delete!"
+        "Nothing found for #{post.title}"
       end
     end
 
@@ -26,12 +39,9 @@ module BBS
       @posts.order
     end
 
-    def assign_owner(user)
-      @post.user_id = user.id
-    end
-
-    def assign_category(category)
-      @post.category_id = category.id
+    private
+    def validate_params(hash)
+      hash.include?('title') and hash.include?('body') and hash.include?('category') and hash.include?('user_id')
     end
   end
 end
